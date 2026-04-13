1d939431bb488c4c591add61e29402c0794fe472
Magnus128 <magu1000@stud.ek.dk>
2026-04-13 11:08:18 +0200

    Card

---
+++ src/Card.java
@@ -0,0 +1,36 @@
+public class Card {
+	private String message;
+	private int income;
+	private int cost;
+	private String event;
+
+	public Card(String message, int income, int cost, String event) {
+		this.message = message;
+		this.income = income;
+		this.cost = cost;
+		this.event = event;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	public int getIncome() {
+		return income;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public String getEvent() {
+		return event;
+	}
+
+	@Override
+	public String toString() {
+		return "Card{" +
+				"message='" + message + '\'' +
+				", income=" + income +
+				", cost=" + cost +
+				", event='" + event + '\'' +
+				'}';
+	}
+}
