ad5eb216ebfff8502d5510efa79ea26fdf13d23f
eminkilic <kilicemin404@gmail.com>
2026-04-15 10:57:55 +0200

    chance

--- src/Chance.java
+++ src/Chance.java
@@ -1,6 +1,27 @@
 public class Chance extends Field{
+private Card card;
 
-    public Chance(int ID, String label){
+    public Chance(int ID, String label, Card card){
         super(ID, label, 0, 0);
+        this.card = card;
+    }
+
+    public String onLand(Player p) {
+        switch (card.getEvent()) {
+            case "pay":
+                Bank.withdraw(cost, p);
+                break;
+            case "receive":
+                Bank.deposit(income,p);
+                break;
+            case "prison": p.updatePosition(11);
+                break;
+            case "wildcard":
+                p.setWildcard(true);
+                break;
+            default:
+                break;
+        }
+        return card.getMessage();
     }
 }
