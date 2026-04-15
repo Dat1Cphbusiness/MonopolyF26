3f6c4727ff51180cb70db1fa5b23a100f6c084f9
phili <phtr1000@stud.ek.dk>
2026-04-15 10:57:56 +0200

    PrisonUpdate

--- src/Prison.java
+++ src/Prison.java
@@ -1,16 +1,42 @@
-public class Prison extends Field implements IOption{
+public class Prison extends Field implements IOption {
 
-    public Prison(int ID, String label, int cost){
+    public Prison(int ID, String label, int cost) {
         super(ID, label, cost, 0);
     }
 
     @Override
     public String onAccept(Player p) {
-        return "";
+        p.setWildcard(false);
+        String msg = "\n Du har kontakter hos Politiet og løslades";
+        return msg;
     }
+
 
     @Override
     public String onReject(Player p) {
-        return "";
+        p.imprision();
+        return "\n Du er kommet i fængsel." +
+                "\n Næste gang det bliver din tur, kan du vælge, om du vil betale dig ud eller prøve at slå et dobbeltslag." +
+                "\n Når du har siddet over i tre omgange, er du løsladt.";
     }
+
+    @Override
+    public String onLand(Player p) {
+
+        String msg = super.onLand(p);
+        msg += "\n Du er arresteret, og skal i fængsel";
+
+        if (p.getWildcard()) {
+            return msg += "\n Vil du bruge dit Wildcard? (Y/N)";
+        } else {
+            p.imprision();
+            return msg += "\n næste gange det bliver din tur, kan du vælge om du vil betale dig ud eller prøve at slå et dobbeltslag.\n" +
+                    " Når du har siddet over i tre omgange er du løsladt";
+
+        }
+
+    }
+
 }
+
+
